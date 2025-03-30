62ec7094d34427d741ae64ab654c574cf2aeed4f
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-03-30 16:31:29 +0200

    Completed E 2. ShouldReturnDept30EmpsOrderedBySalaryDes Test

--- Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -22,7 +22,7 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps.Where(e => e.DeptNo == 30).OrderByDescending(e => e.Sal).ToList(); 
 
         Assert.Equal(2, result.Count);
         Assert.True(result[0].Sal >= result[1].Sal);
