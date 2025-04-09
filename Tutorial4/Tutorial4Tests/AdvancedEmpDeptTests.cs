ba0ff20051b26e5bb995d0ad4acb4e6aa2b70cce
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-04-09 21:32:33 +0200

    test 12 completed

--- Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
+++ Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
@@ -21,7 +21,7 @@ public class AdvancedEmpDeptTests
     {
         var emps = Database.GetEmps();
 
-        decimal? minSalary = null;
+        decimal? minSalary = emps.Where(e => e.DeptNo == 30).Min(e => e.Sal);
 
         Assert.Equal(1250, minSalary);
     }
