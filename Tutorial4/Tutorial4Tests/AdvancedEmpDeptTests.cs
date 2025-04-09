47e61afdc89b309aad8149c9c1022f91f426c714
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-04-09 21:29:59 +0200

    Test 11 completed

--- Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
+++ Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
@@ -9,7 +9,7 @@ public class AdvancedEmpDeptTests
     {
         var emps = Database.GetEmps();
 
-        decimal? maxSalary = null; 
+        decimal? maxSalary = emps.Max(e => e.Sal); 
 
         Assert.Equal(5000, maxSalary);
     }
